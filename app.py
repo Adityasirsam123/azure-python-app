acdbc4fff26014ec6ed14be4fe452dce331efb7c
Aditya  Sirsam <156391626+Adityasirsam123@users.noreply.github.com>
2025-04-20 19:07:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return "Hello from Azure App Service!"
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+#if __name__ == '__main__':
+ #   app.run(host='0.0.0.0')
